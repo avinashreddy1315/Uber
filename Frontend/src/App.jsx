2651ddb74a6a9cfb45aa57035d6d69769b36c264
Ram Naveen Guptha <avinashreddy1315@gmail.com>
2025-01-10 13:20:34 -0500

    Home page design completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import {Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import CaptainSignup from './pages/CaptainSignup'
+import Captainlogin from './pages/Captainlogin'
 const App = () => {
   return (
     <div>
-      App
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<UserLogin/>}/>
+        <Route path='/signup' element={<UserSignup/>}/>
+        <Route path='/captain-login' element={<Captainlogin/>}/>
+        <Route path='/captain-signup' element={<CaptainSignup/>}/>
+      </Routes>
     </div>
   )
 } 

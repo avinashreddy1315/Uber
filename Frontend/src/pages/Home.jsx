7457b9694658c34d852c69478d6e8fce12fb1850
Ram Naveen Guptha <avinashreddy1315@gmail.com>
2025-01-10 13:31:24 -0500

    Home page completed and moving to Userlogin page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import traffiu from '../../public/traffic.webp'
+import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div>
       <div
-        className='h-screen pt-8 flex justify-between flex-col w-full bg-cover bg-center'
+        className='h-screen pt-8 flex justify-between flex-col w-full bg-cover bg-bottom'
         style={{
           backgroundImage: `url(${traffiu})`,
         }}
@@ -12,7 +13,7 @@ const Home = () => {
       <img className='w-14 ml-8'  src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="Uber Logo"/>
             <div className='bg-white pb-7  py-4 px-4'>
              <h2 className='text-3xl font-bold' > Get Started with Uber</h2>    
-             <button className='w-full bg-black text-white py-3 rounded mt-5'> Continue </button> 
+             <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5'> Continue </Link> 
             </div>
       </div>
       

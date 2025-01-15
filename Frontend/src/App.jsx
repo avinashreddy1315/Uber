1bcf59d53c06ebe1dbe2432bd175e55ff25de9bb
Avinahs Reddy <avinashreddy1315@gmail.com>
2025-01-14 20:44:47 -0500

    add the page transistion

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 import Start from './pages/Start';
 import UserLogin from './pages/UserLogin';
 import UserSignup from './pages/UserSignup';
@@ -13,31 +14,51 @@ import UserLogout from './pages/UserLogout';
 import CaptainProtectedWrapper from './components/CaptainProtectedWrapper';
 import CaptainLogout from './pages/CaptainLogout';
 
+const pageTransition = {
+  initial: { opacity: 0, y: 20 },
+  animate: { opacity: 1, y: 0, transition: { duration: 0.2 } },
+  exit: { opacity: 0, y: -20, transition: { duration: 0.2 } }
+};
+
+const AnimatedRoute = ({ element }) => (
+  <motion.div
+    initial="initial"
+    animate="animate"
+    exit="exit"
+    variants={pageTransition}
+  >
+    {element}
+  </motion.div>
+);
+
 const App = () => {
   const ans = useContext(UserDataContext);
+  const location = useLocation();
 
   return (
     <div className='app overflow-x-hidden'>
-      <Routes>
-        {/* Public Routes */}
-        <Route path='/' element={<Start />} />
-        <Route path='/login' element={<UserLogin />} />
-        <Route path='/signup' element={<UserSignup />} />
-        <Route path='/captain-login' element={<Captainlogin />} />
-        <Route path='/captain-signup' element={<CaptainSignup />} />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          {/* Public Routes */}
+          <Route path='/' element={<AnimatedRoute element={<Start />} />} />
+          <Route path='/login' element={<AnimatedRoute element={<UserLogin />} />} />
+          <Route path='/signup' element={<AnimatedRoute element={<UserSignup />} />} />
+          <Route path='/captain-login' element={<AnimatedRoute element={<Captainlogin />} />} />
+          <Route path='/captain-signup' element={<AnimatedRoute element={<CaptainSignup />} />} />
 
-        {/* Protected Routes */}
-        <Route element={<UserProtectedWrapper />}>
-          <Route path='/home' element={<Home/>} />
-          <Route path='/user/logout' element={<UserLogout/>} />
-        </Route>
+          {/* Protected Routes */}
+          <Route element={<UserProtectedWrapper />}>
+            <Route path='/home' element={<AnimatedRoute element={<Home />} />} />
+            <Route path='/user/logout' element={<AnimatedRoute element={<UserLogout />} />} />
+          </Route>
 
-        {/* Captain Routes */}
-        <Route element={<CaptainProtectedWrapper/>}>
-          <Route path='/captain-home' element={<CaptainHome/>} />
-          <Route path='/captain/logout' element={<CaptainLogout/>} />
-        </Route>
-      </Routes>
+          {/* Captain Routes */}
+          <Route element={<CaptainProtectedWrapper />}>
+            <Route path='/captain-home' element={<AnimatedRoute element={<CaptainHome />} />} />
+            <Route path='/captain/logout' element={<AnimatedRoute element={<CaptainLogout />} />} />
+          </Route>
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 };

c57d9f6d0d3471d80a83c5c4bde6e0c6118a88c7
Ram Naveen Guptha <ramkandikatla147@gmail.com>
2025-01-20 19:01:50 -0500

    updated Uber vehicle details in home page

---
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+        {/* This is just a sample data */}
+      <div className='flex gap-4 border-2 p-3 rounded-xl items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'> 24B, Near Kapoor's cafe, Sheryians Coding School, Bhopal</h4>
+      </div>
+
+      <div className='flex gap-4 border-2 p-3 rounded-xl items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'> 24B, Near Kapoor's cafe, Sheryians Coding School, Bhopal</h4>
+      </div>
+
+      <div className='flex gap-4 border-2 p-3 rounded-xl items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'> 24B, Near Kapoor's cafe, Sheryians Coding School, Bhopal</h4>
+      </div>
+
+      <div className='flex gap-4 border-2 p-3 rounded-xl items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'> 24B, Near Kapoor's cafe, Sheryians Coding School, Bhopal</h4>
+      </div>
+
+      <div className='flex gap-4 border-2 p-3 rounded-xl items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'> 24B, Near Kapoor's cafe, Sheryians Coding School, Bhopal</h4>
+      </div>
+
+      <div className='flex gap-4 border-2 p-3 rounded-xl items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'> 24B, Near Kapoor's cafe, Sheryians Coding School, Bhopal</h4>
+      </div>
+    </div>
+  )
+}
+
+export default LocationSearchPanel
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,7 +5,11 @@ import Map from'../../public/image.png'
 import {useGSAP} from '@gsap/react';
 import gsap from 'gsap';
 import 'remixicon/fonts/remixicon.css'
-
+import LocationSearchPanel from '../components/LocationSearchPanel';
+import Ubercar from '../../public/ubercar.webp'
+import ubercar2 from '../../public/ubercar2.webp'
+import ubermotorbike from '../../public/ubermotorbike.webp'
+import uberauto from '../../public/uberauto.webp'
 
 
 const Home = () => {
@@ -26,7 +30,7 @@ const Home = () => {
     {
       gsap.to(panelRef.current,{
         height:'70%',
-        opacity:1
+        padding:24
       })
       gsap.to(panelCloseRef.current,{
         opacity:1
@@ -35,7 +39,7 @@ const Home = () => {
     else
     {
       gsap.to(panelRef.current,{
-        height:'0%',
+        height:'0%'
       })
       gsap.to(panelCloseRef.current,{
         opacity:0
@@ -47,13 +51,13 @@ const Home = () => {
   
 
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative overflow-hidden'>
         <img className='w-16 absolute left-5 top-5 '  src={Uberlogo} alt="Uber Logo"/>
       
       <div className='h-screen w-screen'>
         <img  className='h-full w-full object-cover' src={Map} alt=""></img>
       </div>
-      <div className='flex flex-col justify-end h-screen absolute bottom-0 w-full'>
+      <div className='flex flex-col justify-end h-screen absolute top-0 w-full'>
         <div className='h-[30%] p-6 bg-white relative'>
           <h5 ref={panelCloseRef} onClick={()=>{
             setPanelOpen(false)
@@ -91,8 +95,42 @@ const Home = () => {
           placeholder='Enter your Destination' />
         </form>
         </div>
-        <div ref={panelRef} opacity-0 className='bg-red-500'>
+        <div ref={panelRef} className='bg-white h-0'>
+          <LocationSearchPanel/>
 
+        </div>
+      </div>
+      <div className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-8'> 
+        <h3 className='text-2xl font-semibold mb-5'> Choose a Vehicle</h3>
+
+        <div className='flex border-2 active:border-black mb-2 rounded-xl w-full items-center justify-between p-3'>
+          <img  className='h-10' src={ubercar2} alt=""/>
+          <div className=' ml-2 w-1/2'>
+            <h4 className=' font-medium text-base'> UberGo <span> <i className="ri-user-3-fill"></i>4</span></h4>
+            <h5 className=' font-medium text-base'> 2 mins away</h5>
+            <p className=' font-normal text-xs text-gray-600' > Affordable, compact rides</p>
+          </div>
+          <h2 className='text-lg font-semibold'> $23.20</h2>
+        </div>
+
+        <div className='flex border-2 active:border-black mb-2 rounded-xl w-full items-center justify-between p-3'>
+          <img  className='h-12' src={ubermotorbike} alt=""/>
+          <div className=' ml-2 w-1/2'>
+            <h4 className=' font-medium text-base'> Moto <span> <i className="ri-user-3-fill"></i>1</span></h4>
+            <h5 className=' font-medium text-base'> 3 mins away</h5>
+            <p className=' font-normal text-xs text-gray-600' > Affordable, motorbike rides</p>
+          </div>
+          <h2 className='text-lg font-semibold'> $33.20</h2>
+        </div>
+
+        <div className='flex border-2 active:border-black mb-2 rounded-xl w-full items-center justify-between p-3'>
+          <img  className='h-12' src={uberauto} alt=""/>
+          <div className=' ml-2 w-1/2'>
+            <h4 className=' font-medium text-base'> Uber Auto <span> <i className="ri-user-3-fill"></i>3</span></h4>
+            <h5 className=' font-medium text-base'> 3 mins away</h5>
+            <p className=' font-normal text-xs text-gray-600' > Affordable, Auto rides</p>
+          </div>
+          <h2 className='text-lg font-semibold'> $19.20</h2>
         </div>
       </div>
       
